23cf0e08406492eb69dc41716bf744bad9e38fc4
Dennis Montes <66359525+dennis-montes@users.noreply.github.com>
2025-12-08 12:51:32 +0100

    smooth scroll
    smooth scroll

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,30 @@
 'use client'
 
 import { useEffect } from 'react'
+import Lenis from 'lenis'
 import { initPostHog } from '@/lib/posthog'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     initPostHog()
+
+    const lenis = new Lenis({
+      smoothWheel: true,
+      smoothTouch: false,
+    })
+
+    let frameId = 0
+    const raf = (time: number) => {
+      lenis.raf(time)
+      frameId = requestAnimationFrame(raf)
+    }
+
+    frameId = requestAnimationFrame(raf)
+
+    return () => {
+      cancelAnimationFrame(frameId)
+      lenis.destroy()
+    }
   }, [])
 
   return <>{children}</>
